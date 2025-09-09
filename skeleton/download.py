8067fa6f47f1d02e9319a7547fbe7ddd45af3f4b
chaoyue.song <chaoyue.song@bytedance.com>
2025-09-09 16:54:22 +0800

    fix Michelangelo import issue

--- skeleton/download.py
+++ skeleton/download.py
@@ -3,7 +3,7 @@ from huggingface_hub import hf_hub_download
 file_path = hf_hub_download(
     repo_id="Maikou/Michelangelo",
     filename="checkpoints/aligned_shape_latents/shapevae-256.ckpt",
-    local_dir="third_party/Michelangelo"
+    local_dir="third_partys/Michelangelo"
 )
 
 file_path = hf_hub_download(
